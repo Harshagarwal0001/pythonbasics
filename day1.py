a438d65fded12c92c383997fe9eaab3cd5470043
Harsh Agarwal <Harshagarwal6396@gmail.com>
2024-10-24 18:05:35 +0530

    day1

--- day1.py
+++ day1.py
@@ -35,3 +35,11 @@ print("sum =",a+b)
 print("subtract =",a-b)
 print("multiple =",a*b)
 print("divide =",a/b)
+
+# Check if a number is even or odd
+num = int(input("Enter an integer: "))
+
+if num % 2 == 0:
+    print(num, "is an even number.")
+else:
+    print(num, "is an odd number.")
