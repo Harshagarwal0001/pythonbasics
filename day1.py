b76f26149e774b3af26df720c1d51222f64e2a9b
Harsh Agarwal <Harshagarwal6396@gmail.com>
2024-10-25 23:47:27 +0530

    day1

--- day1.py
+++ day1.py
@@ -1,34 +1,3 @@
-print("hello")
-# Addition of two integers
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-
-result = num1 + num2
-print("The sum of", num1, "and", num2, "is:", result)
-# Subtraction of two integers
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-
-result = num1 - num2
-print("The difference between", num1, "and", num2, "is:", result)
-# Division of two integers
-num1 = int(input("Enter the numerator: "))
-num2 = int(input("Enter the denominator: "))
-
-# Check to avoid division by zero
-if num2 != 0:
-    result = num1 / num2
-    print(num1, "divided by", num2, "is:", result)
-else:
-    print("Division by zero is not allowed.")
-# Multiplication of two integers
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-
-result = num1 * num2
-print("The product of", num1, "and", num2, "is:", result)
-
-
 a = int(input("num1 = "))
 b = int(input("num2 = "))
 print("sum =",a+b)
@@ -44,4 +13,3 @@ if num % 2 == 0:
 else:
     print(num, "is an odd number.")
 
-print ("no")
