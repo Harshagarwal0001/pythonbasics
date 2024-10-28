3df45c05ba5103c7d3ea6a451aee8896bd1bb2e9
Harsh Agarwal <Harshagarwal6396@gmail.com>
2024-10-28 19:08:51 +0530

    Q1

---
+++ .idea/140 Question/Q2.py
@@ -0,0 +1,9 @@
+# 2. Write a Python program to do arithmetical operations addition and division
+a = 10
+b = 5
+addition_result = a + b
+print("Addition:", addition_result)
+
+# Perform division
+division_result = a / b
+print("Division:", division_result)
