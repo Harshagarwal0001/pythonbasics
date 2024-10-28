f1dfa28e691ea2c4f23de522c6426af896694fe5
Harsh Agarwal <Harshagarwal6396@gmail.com>
2024-10-28 18:56:12 +0530

    Q1

--- day2.py
+++ day2.py
@@ -1,1 +1,19 @@
-print("hello")
+#Q1 :-  Extracting a Substring:
+# Given the string "Hello, World!", extract the substring "World" using slicing.
+greeting = "Hello, World!"
+world_substring = greeting[7:12]
+print(world_substring)
+
+#Q2 :-Reversing a String:
+# Write a slice operation to reverse the string "Python".
+language = "Python"
+reversed_language = language[::-1]
+print(reversed_language)
+
+#Q3 :- Skipping Characters:
+# From the string "abcdefgh", extract every second character starting from the first.
+a = "abcdefgh"
+print(a[::2])
+
+#Q4 :- First and Last Characters:
+# Slice and print the first and last characters from the string "SDET".
