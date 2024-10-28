aa6385e157de3a635639f9cb506dfe1433ac37f6
Harsh Agarwal <Harshagarwal6396@gmail.com>
2024-10-28 19:25:22 +0530

    day2

---
+++ 4.py
@@ -0,0 +1,9 @@
+# Q4:-  Write a Python program to swap two variables.
+a = input("Enter the value of a: ")
+b = input("Enter the value of b: ")
+
+a, b = b, a
+
+print("After swapping:")
+print("a =", a)
+print("b =", b)
---
+++ Q5.py
@@ -0,0 +1,6 @@
+# Q5 :-  Write a Python program to generate a random number.
+import random
+
+random_number = random.randint(1, 10)
+
+print("Random number:", random_number)
