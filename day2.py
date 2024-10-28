df5a02025af234f3c3f4a55efbd809dcb3bde0bd
Harsh Agarwal <Harshagarwal6396@gmail.com>
2024-10-28 19:05:12 +0530

    day2

--- day2.py
+++ day2.py
@@ -17,3 +17,11 @@ print(a[::2])
 
 #Q4 :- First and Last Characters:
 # Slice and print the first and last characters from the string "SDET".
+a = "SDET"
+print(a[0] + a[-1])
+
+#Q5 :-  Remove First n Characters:
+# Given the string "OpenAI", write a slice operation to remove the first 2 characters.\
+a = "OpenAI"
+result = a[2:]
+print(result)
